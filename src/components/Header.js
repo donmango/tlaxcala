e7549c055f32b52fa588375ffd5e575c6e76535c
Miguel Palau <mike3run@users.noreply.github.com>
2017-04-26 22:11:32 -0500

    better background

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 export default styled.header`
   align-items: center;
   background: url(./images/background-header.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
   display: flex;
   padding: 6em 0;
 `
